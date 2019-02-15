23c3e722d3e956e878c9c38def8708e5df37e836
Arivin Nair <arivin.nair@gmail.com>
2019-02-14 17:08:04 -0800

    Created amazon assign1_2_3_feb14

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get "home", to: "welcome#home"
+
+  get "about", to: "welcome#about"
+
+  get "contact_us", to: "welcome#contact_us"
+
+  post "process_contact", to: "welcome#process_contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
